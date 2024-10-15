65b6eb6d3e97f6c2b2e58d381f93bffedbfb83e2
Saba Shagidze <154977694+SabaShagidzeMain@users.noreply.github.com>
2024-10-15 09:05:08 +0000

    switch-case, Tasks

--- LecOne.cpp
+++ LecOne.cpp
@@ -63,5 +63,71 @@ int main()
     cin >> user_input;
     cout << "You Said: " << user_input << endl;
 
+    // Switch Case Statements
+    switch (i)
+    {
+    case 1:
+        cout << "Only One?" << endl;
+        break;
+    case 2:
+        cout << "I Want More" << endl;
+        break;
+    case 3:
+        cout << "Not Bad" << endl;
+        break;
+    case 4:
+        cout << "Ok" << endl;
+        break;
+    default:
+        cout << "Default Output" << endl;
+    }
+
+    // Task 1.1
+    cout << "===========================================" << endl;
+    cout << "Task 1.1" << endl;
+    cout << "Saba Shagidze" << endl;
+    cout << "21" << endl;
+    cout << "Georgia" << endl;
+
+    // Task 1.2
+    cout << "===========================================" << endl;
+    cout << "Task 1.2" << endl;
+    int age = 21;
+    cout << "Age is: " << age << endl;
+
+    // Task 1.5
+    cout << "===========================================" << endl;
+    cout << "Task 1.5" << endl;
+    int phone_number = 577353535;
+    double id_number = 1405045666;
+    char gender = 'M';
+    cout << phone_number << " " << id_number << " " << gender << endl;
+
+    // Task 1.6
+    cout << "===========================================" << endl;
+    cout << "Task 1.6" << endl;
+    int date;
+    int month;
+    int year;
+    cout << "Enter Date/Month/Year:";
+    cin >> date >> month >> year;
+
+    if (month >= 1 && month <= 12)
+    {
+        cout << date << "/" << month << "/" << year << endl;
+    }
+    else
+    {
+        cout << "incorrect month" << endl;
+    }
+
+    // Task 1.7
+    cout << "===========================================" << endl;
+    cout << "Task 1.6" << endl;
+    int threeDigit;
+    cout << "Write A Three Digit Number" << endl;
+    cin >> threeDigit;
+    cout << threeDigit / 100 * 100 << "+" << threeDigit / 10 % 10 * 10 << "+" << threeDigit % 10 << endl;
+
     return 0;
 }
