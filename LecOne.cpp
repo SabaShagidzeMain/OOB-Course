944e3a59eee7febf670562e1861428178152ca3b
Saba Shagidze <154977694+SabaShagidzeMain@users.noreply.github.com>
2024-10-15 07:23:53 +0000

    Increment-Decrement

--- LecOne.cpp
+++ LecOne.cpp
@@ -2,17 +2,16 @@
 
 using namespace std;
 
+// git add .
+// git commit -m "comment"
+// git push
+
 int main()
 {
     cout << "Hello World!" << endl;
 
     int counter;
     counter = 13;
-
-    // Same (variable + 1)
-    counter = counter + 1;
-    counter++;
-    cout << counter << endl;
 
     int i, j, k, l;
 
@@ -32,6 +31,13 @@ int main()
     r = 5;
     t = f % r;
     cout << r << endl;
+
+    // Increment & Decrement
+    int sheep_counter;
+    sheep_counter = 0;
+    sheep_counter++; // Increment (+1)
+    sheep_counter--; // Decrement (-1)
+    cout << sheep_counter << endl;
 
     return 0;
 }
