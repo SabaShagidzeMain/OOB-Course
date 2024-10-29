006b1096e404053116be2d8bdc4b7534f809528f
Saba Shagidze <154977694+SabaShagidzeMain@users.noreply.github.com>
2024-10-29 07:23:36 +0000

    Quiz 2 Answer

--- Quiz2.cpp
+++ Quiz2.cpp
@@ -1,1 +1,39 @@
+#include <iostream>
 
+using namespace std;
+
+int main()
+{
+   int userDegree;
+   cout << "Enter Temp" << endl;
+   cin >> userDegree;
+
+   if (userDegree < 0)
+   {
+      cout << "Freezing:" << userDegree << "Deg" << endl;
+   }
+   else if (userDegree < 10)
+   {
+      cout << "Very Cold:" << userDegree << "Deg" << endl;
+   }
+   else if (userDegree < 20)
+   {
+      cout << "Cold:" << userDegree << "Deg" << endl;
+   }
+   else if (userDegree < 30)
+   {
+      cout << "Normal:" << userDegree << "Deg" << endl;
+   }
+   else if (userDegree < 40)
+   {
+      cout << "Hot:" << userDegree << "Deg" << endl;
+   }
+   else if (userDegree > 40)
+   {
+      cout << "Very Hot:" << userDegree << endl;
+   }
+   else
+   {
+      cout << "Default Error";
+   }
+}
